5804960a43b4ea697ff99497ce01f759df37cea5
Jacobo Vazquez <vazquezmjacobo@gmail.com>
2024-07-13 22:42:21 -0400

    Initial Commit

---
+++ PowerFounction/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PowerFounction
+//
+//  Created by Jacobo Vazquez on 7/13/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
